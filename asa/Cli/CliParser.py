0384010f7c9efb9a0103bdc54f5fd4280ea9f674
Amaury Guillermin <amauryguillermin@gmail.com>
2020-03-24 23:56:44 +0100

    Cli feature added

---
+++ asa/Cli/CliParser.py
@@ -0,0 +1,12 @@
+from asa.AnsibleAssembler import AnsibleAssembler
+
+class CliParser(AnsibleAssembler):
+    def __init__(self, path, config, action, resource, **args):
+        super().__init__(path, config)
+        self.action = action
+        self.resource = resource
+        self.args = args
+
+    def runCommand(self):
+        methodName = ''.join([self.action, self.resource.capitalize()])
+        getattr(self, methodName)(name=self.args["name"])
