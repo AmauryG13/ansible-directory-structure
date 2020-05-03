4cbe73863ea7b5fd24c48a30778125c94631d405
Amaury Guillermin <amauryguillermin@gmail.com>
2020-05-03 22:44:06 +0200

    Configuration management

---
+++ src/ads/core/config/manager.py
@@ -0,0 +1,69 @@
+import os
+from ads.core.parser.yaml import Yaml
+
+class Manager(Yaml):
+    _defaultPath = os.path.join(os.path.dirname(__file__), '../../../../config')
+    _configKeys = ['layout', 'inventory', 'playbook', 'role', 'config'];
+
+    def __init__(self, configPath=None):
+        super().__init__()
+        self.configPath = configPath
+
+    def readDefaultConfig(self):
+        content = self._readConfigFiles(self._configKeys, self._defaultPath)
+
+        self.config = content['layout']['default']
+        for inKey in self.config:
+            self.config[inKey] = content[inKey]
+
+    def readUsertConfig(self):
+        files = self._scanForConfigFile(self.configPath)
+        keys = self._getKeysForFiles(files)
+
+        content = self._readConfigFiles(keys, self.configPath)
+        return content
+
+    def mergeConfigContent(self, content):
+        for userKey in content:
+            self.config[userKey] = content[userKey]
+
+    def readConfig(self):
+        self.readDefaultConfig()
+
+        if self.configPath is not None:
+            userConfig = self.readUsertConfig(self.configPath)
+            self.mergeConfigContent(userConfig)
+
+    def _readConfigFiles(self, keys, path):
+        content = {}
+        for key in keys:
+            [content[key], file] = self.parseYamlFile(
+                self._getConfigFile(key),
+                self._defaultPath)
+
+        return content
+
+    def _scanForConfigFile(self, path=None):
+        if path is None:
+            path = self.defaultPath
+
+        return os.listdir(path)
+
+    def _getConfigFile(self, filename):
+        configFile = filename + '.yaml'
+        return configFile
+
+    def _getKeysFromFiles(self, files):
+        keys = []
+        for file in files:
+            keys.append(file[0:file.find('.')])
+
+        return keys
+
+if __name__ == '__main__':
+    import pprint
+
+    config = Manager()
+    config.readConfig()
+
+    pprint.pprint(config.config)
