99ffbe14abe96dd06014be30b572ce4c3f8a7e11
Amaury Guillermin <amauryguillermin@gmail.com>
2020-05-05 15:57:38 +0200

    Flake8ing setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-import os, setuptools
+from os import path
+import setuptools
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-def get_metadata():
+
+def get_metadata(source):
     """Get version and version_info from ads/__meta__.py file."""
-    module_path = os.path.join(os.path.dirname('__file__'), 'src/ads', '__meta__.py')
+    module_path = path.join(path.dirname('__file__'), source, '__meta__.py')
 
     import importlib.util
     spec = importlib.util.spec_from_file_location('__meta__', module_path)
@@ -13,18 +16,19 @@ def get_metadata():
     spec.loader.exec_module(meta)
     return meta.__version__, meta.__author__, meta.__package__
 
-__version__, __author__, __package__ = get_metadata()
+
+__version__, __author__, __package__ = get_metadata('src/ads')
 
 setuptools.setup(
     name=__package__,
     version=__version__,
     author=__author__,
     author_email="amauryguillermin@gmail.com",
-    description="Package to generate the convinent Ansible directory structure",
+    description="Package to generate convinent Ansible directory structure",
     long_description=long_description,
     long_description_content_type="text/markdown",
     scripts=['bin/ansible-structure'],
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
     classifiers=[
         "Programming Language :: Python :: 3",
