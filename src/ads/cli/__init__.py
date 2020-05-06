c903e66e36d5e049af58b0d6da74808b89072a9a
Amaury Guillermin <amauryguillermin@gmail.com>
2020-05-06 17:43:08 +0200

    Plug builder in cli

--- src/ads/cli/__init__.py
+++ src/ads/cli/__init__.py
@@ -32,8 +32,11 @@ class Cli(object):
         self._createSubparsers()
 
     def parse(self):
-        args = self.parser.parse_args()
-        print(args)
+        self.args = self.parser.parse_args()
+        print(self.args)
+
+        args = self.__normalizeArgs()
+        return args
 
     def _createSubparsers(self):
         self.subparsers = self.parser.add_subparsers(
@@ -52,131 +55,147 @@ class Cli(object):
         self._parserRole()
 
     def _parserRepo(self):
-        self.subparser['repo'] = self.subparsers.add_parser(
+        key = 'repo'
+
+        self.subparser[key] = self.subparsers.add_parser(
             'repo',
             formatter_class=argparse.RawDescriptionHelpFormatter,
             description='Create a new repository for Ansible',
             epilog=epilog
         )
 
-        self.subparser['repo'].add_argument(
-            '--mode', '-m',
-            dest='mode',
-            metavar='MODE',
-            nargs=1,
-            help='specify the mode used to create the resource',
-            choices=('default', 'alternative')
-        )
+        self.__setModeArgument(key)
 
-        self.subparser['repo'].add_argument(
+        self.subparser[key].add_argument(
             '--path,-p',
             dest='path',
             metavar='PATH',
-            help='specify the relative path from where the command is launched to create the resource',
+            help='specify the relative path from where the command is launched to create the repo',
         )
 
-        self.subparser['repo'].add_argument(
+        self.subparser[key].add_argument(
             '--extra',
-            help='specify if the repositoty should be created with its extra dependencies',
-            action='store_true'
+            dest='options',
+            const='extra',
+            help='specify if the repo should be created with its extra dependencies',
+            action='append_const'
         )
 
     def _parserConfig(self):
-        self.subparser['config'] = self.subparsers.add_parser(
+        key = 'config'
+
+        self.subparser[key] = self.subparsers.add_parser(
             'config',
             formatter_class=argparse.RawDescriptionHelpFormatter,
             description='Create a new configuration',
             epilog=epilog
         )
 
-        self.subparser['config'].add_argument(
-            '--mode,-m',
-            dest='mode',
-            metavar='MODE',
-            help='specify the mode used to create the resource',
-            choices=('default', 'alternative')
-        )
+        self.__setModeArgument(key)
 
     def _parserInventory(self):
-        self.subparser['inventory'] = self.subparsers.add_parser(
+        key = 'inventory'
+
+        self.subparser[key] = self.subparsers.add_parser(
             'inventory',
             formatter_class=argparse.RawDescriptionHelpFormatter,
             description='Create a new inventory',
             epilog=epilog
         )
 
-        self.subparser['inventory'].add_argument(
+        self.subparser[key].add_argument(
             'name',
             help='specify the name of the inventory'
         )
 
-        self.subparser['inventory'].add_argument(
-            '--mode,-m',
-            metavar='MODE',
-            help='specify the mode used to create the resource',
-            choices=('default', 'alternative')
-        )
+        self.__setModeArgument(key)
 
     def _parserPlaybook(self):
-        self.subparser['playbook'] = self.subparsers.add_parser(
+        key = 'playbook'
+        self.subparser[key] = self.subparsers.add_parser(
             'playbook',
             formatter_class=argparse.RawDescriptionHelpFormatter,
             description='Create a new playbok',
             epilog=epilog
         )
 
-        self.subparser['playbook'].add_argument(
+        self.subparser[key].add_argument(
             'name',
             help='specify the name of the playbook'
         )
 
-        self.subparser['playbook'].add_argument(
-            '--mode,-m',
-            metavar='MODE',
-            help='specify the mode used to create the resource',
-            choices=('default', 'alternative')
-        )
+        self.__setModeArgument(key)
 
     def _parserRole(self):
-        self.subparser['role'] = self.subparsers.add_parser(
+        key = 'role'
+        self.subparser[key] = self.subparsers.add_parser(
             'role',
             formatter_class=argparse.RawDescriptionHelpFormatter,
             description='Create a new role directory',
             epilog=epilog
         )
 
-        self.subparser['role'].add_argument(
+        self.subparser[key].add_argument(
             'name',
             help='specify the name of the role'
         )
 
-        self.subparser['role'].add_argument(
-            '--mode,-m',
+        self.__setModeArgument(key)
+
+        self.subparser[key].add_argument(
+            '--files',
+            dest='options',
+            const='files',
+            help='specify if the role should be created with the files directories',
+            action='append_const'
+        )
+
+        self.subparser[key].add_argument(
+            '--variables',
+            dest='options',
+            const='variables',
+            help='specify if the role should be created with the variables directories',
+            action='append_const'
+        )
+
+        self.subparser[key].add_argument(
+            '--extra',
+            dest='options',
+            const='extra',
+            help='specify if the role should be created with its extra dependencies',
+            action='append_const'
+        )
+
+        self.subparser[key].add_argument(
+            '--all',
+            dest='options',
+            const=['files', 'variables', 'extra'],
+            help='specify if the role should be created with all its dependencies',
+            action='store_const'
+        )
+
+    def __setModeArgument(self, key):
+        help = 'specify the mode used to create the {}'.format(key)
+
+        self.subparser[key].add_argument(
+            '--mode', '-m',
+            dest='mode',
             metavar='MODE',
-            help='specify the mode used to create the resource',
+            nargs=1,
+            type=str,
+            default='default',
+            help=help,
             choices=('default', 'alternative')
         )
 
-        self.subparser['role'].add_argument(
-            '--files',
-            help='specify if the role should be created with the files directories',
-            action='store_true'
-        )
+    def __normalizeArgs(self):
+        keys = ['path', 'mode', 'config', 'name', 'options']
+        args = vars(self.args)
 
-        self.subparser['role'].add_argument(
-            '--variables',
-            help='specify if the role should be created with the variables directories',
-            action='store_true'
-        )
+        args['mode'] = args['mode'][0]
 
-        self.subparser['role'].add_argument(
-            '--extra',
-            help='specify if the role should be created with its extra dependencies',
-            action='store_true'
-        )
+        for key in keys:
+            if key not in args:
+                args[key] = None
 
-        self.subparser['role'].add_argument(
-            '--all',
-            help='specify if the role should be created with all its dependencies',
-            action='store_true'
-        )
+        return args
--- src/ads/cli/__main__.py
+++ src/ads/cli/__main__.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 from ads.cli import Cli
+from ads.__main__ import Builder
+
+
+def getArgs(method, args):
+    output = {}
+    keys = {
+        'init': ['path', 'mode', 'config'],
+        'hook': ['name']
+    }
+
+    for key in keys[method]:
+        if key in args:
+            output[key] = args[key]
+
+    return output
+
 
 cli = Cli()
 cli.init()
 
-cli.parse()
+args = cli.parse()
+print(args)
+
+ads = Builder(**getArgs('init', args))
+
+funcName = 'create{}'.format(args['resource'].capitalize())
+getattr(ads, funcName)(**getArgs('hook', args))
